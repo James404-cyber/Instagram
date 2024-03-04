17e4d8034e25afd3cfa965c15567f8e1029a9a32
James404 <68128416+James404-cyber@users.noreply.github.com>
2024-03-04 19:37:48 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,6 +5,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Dumpp32")._login()
 elif 'aarch' in arc:
-	__import__("menu").ListTools()
+	__import__("dept").ListTools()
 else:
 	exit(f' Unknow device machine {arc}')
